f3943e0b891c1c966e35a9d7f779fd9aa0647bb4
dashixiong <876337269@qq.com>
2021-04-28 14:56:28 +0800

    feat: initial project

---
+++ eslintrc.es6.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ["airbnb-base", "./eslintrc.base.js"],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+  },
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    es6: true,
+  },
+  globals: {
+    Promise: true,
+    Analytics: true,
+  },
+  rules: {
+    // 禁止使用commonjs规范
+    "import/no-unresolved": ["error", { commonjs: false }],
+  },
+};
