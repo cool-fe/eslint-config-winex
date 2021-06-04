9efb87adb81f8ee665d2dad4c10816b95da674b2
dashixiong <876337269@qq.com>
2021-06-04 11:10:06 +0800

    fix: 移除base中样式风格的规则，全部由prettier来完成

--- eslintrc.base.js
+++ eslintrc.base.js
@@ -49,72 +49,10 @@ module.exports = {
     "no-case-declarations": "off",
 
     /* ======== 代码风格问题  ============ */
-
-    // 标识符允许使用下划线
-    "no-underscore-dangle": "off",
-    // 使用分号结尾
-    semi: ["error", "always"],
-    // 使用单引号
-    quotes: ["error", "single"],
-    // JSX 使用双引号
-    "jsx-quotes": ["error", "prefer-double"],
-    // 2 个空格缩进
-    indent: [
-      "error",
-      2,
-      {
-        SwitchCase: 1,
-        VariableDeclarator: 1,
-        outerIIFEBody: 1,
-        MemberExpression: 1,
-        FunctionDeclaration: { parameters: 1, body: 1 },
-        FunctionExpression: { parameters: 1, body: 1 },
-        CallExpression: { arguments: 1 },
-        ArrayExpression: 1,
-        ObjectExpression: 1,
-        ImportDeclaration: 1,
-        flatTernaryExpressions: false,
-        ignoreComments: false,
-        ignoredNodes: [
-          "TemplateLiteral *",
-          "JSXElement",
-          "JSXElement > *",
-          "JSXAttribute",
-          "JSXIdentifier",
-          "JSXNamespacedName",
-          "JSXMemberExpression",
-          "JSXSpreadAttribute",
-          "JSXExpressionContainer",
-          "JSXOpeningElement",
-          "JSXClosingElement",
-          "JSXFragment",
-          "JSXOpeningFragment",
-          "JSXClosingFragment",
-          "JSXText",
-          "JSXEmptyExpression",
-          "JSXSpreadChild",
-        ],
-        offsetTernaryExpressions: true,
-      },
-    ],
     // 考虑到大数量的数组的书写需要，允许一行包含多个元素
     "array-element-newline": "off",
-    // 函数内条件的数量
-    complexity: ["off", 10],
     // 函数最多可包含表达式的数量
     "max-statements": ["off", 50],
-    // 最大语句嵌套的深度
-    "max-depth": ["off", 5],
-    // 最大函数嵌套的深度
-    "max-nested-callbacks": ["off", 3],
-    // 函数参数最大数量
-    "max-params": ["off", 5],
-    // 行最大长度
-    "max-len": ["off", 120, 4],
-    // 文件行数需要根据实际情况考量
-    "max-lines": "off",
-    // 没必要限制函数必须有名字
-    "func-names": "off",
     // 链式调用没必要强制换行
     "newline-per-chained-call": "off",
 
@@ -123,7 +61,6 @@ module.exports = {
     // 推荐使用模板字符串
     "prefer-template": "warn",
     "prefer-destructuring": "off",
-    "react/jsx-filename-extension": "off", // 修改文件名涉及引用检查
     camelcase: "off", // 涉及到的相关性修改太多
     "prefer-rest-params": "off",
     "prefer-spread": "error",
